703f3fa07b183b2e895c5fa6db89bbb0ba942a87
The Cloud Maker <jdluther2020@gmail.com>
2021-04-13 10:42:49 -0700

    Lambda invoke loop 30 times

--- cli-lambda-invoke-loop.sh
+++ cli-lambda-invoke-loop.sh
@@ -1,7 +1,7 @@
 sudo yum install -y jq
 
 counter=1
-while [ $counter -le 15 ]
+while [ $counter -le 30 ]
 do
   aws lambda invoke --function-name \
   $(aws lambda list-functions | jq -r -c '.Functions[] | select( .FunctionName | contains("lambda-canary-app-HelloWorldFunction")).FunctionName'):live \
